86d351dcba17a5f5ebf2080dbf6b281da4a67339
Artur Linnik <50211406+ArturLinnik@users.noreply.github.com>
2023-03-01 12:12:47 +0100

    [Add] Added aws channels as comments

--- web-ui/src/config.js
+++ web-ui/src/config.js
@@ -6,5 +6,9 @@
 export const PLAYBACK_URL_1 = "https://fcc3ddae59ed.us-west-2.playback.live-video.net/api/video/v1/us-west-2.893648527354.channel.DmumNckWFTqz.m3u8";
 export const PLAYBACK_URL_2 = "https://fcc3ddae59ed.us-west-2.playback.live-video.net/api/video/v1/us-west-2.893648527354.channel.DmumNckWFTqz.m3u8";
 
+// Channels
+// https://640e5263ced1.eu-west-1.playback.live-video.net/api/video/v1/eu-west-1.041211539744.channel.nJist39MAWQ7.m3u8
+// https://640e5263ced1.eu-west-1.playback.live-video.net/api/video/v1/eu-west-1.041211539744.channel.4MdMdtefqYMr.m3u8
+
 // Chat websocket address
 export const CHAT_WEBSOCKET = "wss://edge.ivschat.eu-west-1.amazonaws.com";
