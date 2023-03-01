0cb637aec363466df496ddb0e1159ecb0f23e727
jose-cartagena-bemyvega <124348227+jose-cartagena-bemyvega@users.noreply.github.com>
2023-03-01 12:25:43 +0100

    Update config.js

--- web-ui/src/config.js
+++ web-ui/src/config.js
@@ -11,4 +11,4 @@ export const PLAYBACK_URL_2 = "https://fcc3ddae59ed.us-west-2.playback.live-vide
 // https://640e5263ced1.eu-west-1.playback.live-video.net/api/video/v1/eu-west-1.041211539744.channel.4MdMdtefqYMr.m3u8
 
 // Chat websocket address
-export const CHAT_WEBSOCKET = "wss://edge.ivschat.eu-west-1.amazonaws.com";
+export const CHAT_WEBSOCKET = "wss://yjc4q0fef3.execute-api.eu-west-1.amazonaws.com/Prod";
